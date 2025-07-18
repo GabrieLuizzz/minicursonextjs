ed127b4e31665023c27c00fcc688a1946463fe2c
Gabriel Luiz da Silva Pena <gabrieluizzz11@gmail.com>
2025-07-18 13:39:58 -0300

    penúltima aula do minicursonextjs.

---
+++ src/app/membros/page.tsx
@@ -0,0 +1,12 @@
+
+import { window } from "../components/window";
+
+export default function membros() {
+  return (
+      <div>
+          <window>
+              oi
+          </window>
+      </div>>
+  );
+}
